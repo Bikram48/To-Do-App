47307a5b145dd2617c806ae5ca69a6e181ae02b1
Bikram48 <chandbikram001@gmail.com>
2021-12-27 13:13:16 +0545

    Page Layout The overall design of the app has been completed.Now I am currently working on the backend of the app(CRUD operation on task) using PHP.

--- pages/home.php
+++ pages/home.php
@@ -4,17 +4,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/home.css"/>
+    <link rel="stylesheet" href="../styles/home.css"/>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <title>Document</title>
 </head>
 <body>
     <div class="container">
-        <h2>To-do-Task App</h2>
+        <h2 style="font-size:20px; font-weight: bolder;">To-do-Task App</h2><br>
         <form action="<?php echo $_SERVER['PHP_SELF'];?>" >
             <div class="row">
-                <div class="col-xl-8">
+                <div class="col-xl-4">
                     <input type="email" class="form-control" id="email" placeholder="Enter Task" name="email">
                 </div>
                 <div class="col-sm-4 col-xl-4 ">
@@ -23,10 +23,32 @@
              
             </div>
            
-        </form><br> <br>
-        <div class="form-group">   
+        </form><br> 
+        <div style="margin-left:20px;" class="form-group">   
             <div class="row">   
-                <div class="col-sm-offset-2 col-xl-6">
+                <div class="col-sm-offset-2 col-xl-4">
+                    <div class="checkbox">
+                        <label><input type="checkbox" name="remember"> Task 1</label>
+                    </div>
+                </div>
+                <div class="col-xl-2">
+                    <i class="fas fa-edit"></i>
+                    <i class="fas fa-times"></i>
+                </div>
+            </div>  
+            <div class="row">   
+                <div class="col-sm-offset-2 col-xl-4">
+                    <div class="checkbox">
+                        <label><input type="checkbox" name="remember"> Task 1</label>
+                    </div>
+                </div>
+                <div class="col-xl-2">
+                    <i class="fas fa-edit"></i>
+                    <i class="fas fa-times"></i>
+                </div>
+            </div>  
+            <div class="row">   
+                <div class="col-sm-offset-2 col-xl-4">
                     <div class="checkbox">
                         <label><input type="checkbox" name="remember"> Task 1</label>
                     </div>
